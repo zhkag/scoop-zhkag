60ed8b9ff9610219420c5fea1891242588661a67
zhkag <zhkag@foxmail.com>
2023-10-13 20:49:34 +0800

    Synchronization Code

--- bin/reset-office-version.ps1
+++
@@ -1,7 +0,0 @@
-$dir = "$psscriptroot\..\bucket"
-
-$office = "$dir\office-365-apps-np.json"
-(Get-Content "$office").Replace(': "office"', ': "0"') | Set-Content -Path "$office"
-
-$office = "$dir\office-365-apps-minimal-np.json"
-(Get-Content "$office").Replace(': "office"', ': "0"') | Set-Content -Path "$office"
